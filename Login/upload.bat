091165954edce9d3575ed766bb41532125e9d4af
Quinn <quinnhenning1@gmail.com>
2024-12-30 13:15:23 +0100

    Automatischer Commit

--- Login/upload.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "C:\Users\quinn\OneDrive\Dokumente\programm\"  // Ersetze den Pfad mit dem richtigen Pfad zu deinem Repository
-git status
-git add .
-git commit -m "Automatischer Commit"
-git push origin master
-pause
